c767db99d2db310c02a98f2380e529dec4963505
Sean Liu (Oculus) <sean.liu@oculus.com>
2019-01-26 12:40:01 -0800

    restart

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public Canvas dialogueCanvas;
     public string startText = "The story of a founder building the Uber for Crypto";
+    public string loseText = "You ran out of money. Start a new company?";
+    private bool isAlive = true;
+
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +19,8 @@ public class GameManager : MonoBehaviour
 
     void PlayerController_OnDied()
     {
-        dialogueCanvas.GetComponent<DialogueController>().ShowText("YOU DEAD", 6.0f);
+        dialogueCanvas.GetComponent<DialogueController>().ShowText(loseText, 999.0f);
+        isAlive = false;
     }
 
     private void OnDestroy()
@@ -41,7 +46,13 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (!isAlive)
+        {
+            if (Input.GetButtonUp("Submit"))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
     }
 
 
