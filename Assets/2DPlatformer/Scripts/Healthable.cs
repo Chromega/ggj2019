bb8441fb0938c6350cb631dac88a24bcb9fb75d9
Amber Feng <amber.feng@gmail.com>
2019-01-26 00:18:05 -0800

    Damage counters

--- Assets/2DPlatformer/Scripts/Healthable.cs
+++ Assets/2DPlatformer/Scripts/Healthable.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Healthable : MonoBehaviour
 {
@@ -32,10 +33,42 @@ public class Healthable : MonoBehaviour
 
         // Flash red on the sprite for damage taken
         StartCoroutine("FlashDamageAnimation");
+
         if (movable) movable.Recoil((transform.position - g.transform.position).normalized);
+
+        // Adding a damage marker
+        GameObject damageMarkerPrefab = Resources.Load<GameObject>("DamageCounter");
+        GameObject damageMarker = Instantiate(damageMarkerPrefab); 
+
+        //damageMarker.transform.SetParent(this.transform);
+        damageMarker.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + 0.8f, -2);
+        damageMarker.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
+
+        TextMesh text = damageMarker.GetComponent<TextMesh>();
+        text.text = "-" + damage.ToString();
+
+        StartCoroutine(FadeDamageMarker(damageMarker));
 
         PlayerController player = GetComponent<PlayerController>();
         if (player) player.updateFundsLeft(health);
+    }
+
+    IEnumerator FadeDamageMarker(GameObject damageMarker)
+    {
+        // Float up and fade out
+        for (float f = 1f; f >= 0; f -= 0.1f)
+        {
+            damageMarker.transform.position = new Vector3(damageMarker.transform.position.x, damageMarker.transform.position.y + 0.1f, -2);
+            Debug.Log(damageMarker.transform.position);
+            Debug.Log(damageMarker.transform.localPosition);
+
+            TextMesh text = damageMarker.GetComponent<TextMesh>();
+            text.color = new Color(text.color.r, text.color.g, text.color.b, text.color.a - 0.05f);
+            Debug.Log(text.color);
+            yield return new WaitForSeconds(.05f);
+        }
+
+        Destroy(damageMarker);
     }
 
     IEnumerator FlashDamageAnimation()
