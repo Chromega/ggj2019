be750cca574b85a4ba1504cd3f3dbcebe769b7aa
Mark Sullivan <markasullivaniii@gmail.com>
2019-01-26 11:31:22 -0800

    Dialogue Controller

---
+++ Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public string text;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (DialogueController.Instance)
+        {
+            DialogueController.Instance.ShowText(text);
+        }
+        else
+        {
+            Debug.LogError("Null dialogue controller");
+        }
+    }
+}
