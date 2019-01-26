e22e1686e8bd8087a75a0d9eddaa02b6fd620707
Amber Feng <amber.feng@gmail.com>
2019-01-25 19:22:05 -0800

    Light enemy AI - they walk toward you when you get inside vision range.

--- Assets/2DPlatformer/Scripts/EnemyPatrol.cs
+++ Assets/2DPlatformer/Scripts/EnemyPatrol.cs
@@ -3,7 +3,10 @@
 public class EnemyPatrol : Movable
 {
 
+    public PlayerController player; 
+    public float visionRange = 5; 
     private float direction = -1.0f;
+
 
     void Awake()
     {
@@ -18,11 +21,24 @@ public class EnemyPatrol : Movable
 
     protected override float getHorizontalDirection()
     {
+        // Patrol in a random direction
         float probability = Random.Range(0.0f, 1.0f);
         if (probability > 0.97f)
         {
             direction *= -1;
         }
+
+        // If you see the player, walk toward the player
+        Vector2 playerPosition = player.transform.position;
+        Vector2 position = this.transform.position;
+
+        if (Mathf.Abs(playerPosition.x - position.x) < visionRange)
+        {
+            Debug.Log("I SEE YOU");
+            direction = (playerPosition.x - position.x) / (Mathf.Abs(playerPosition.x - position.x));
+        }
+
+
 
         return direction;
     }
