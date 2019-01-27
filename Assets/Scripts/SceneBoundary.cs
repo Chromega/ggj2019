de74419c533e85758fbb5c633853ff5f7a614837
Sean Liu (Oculus) <sean.liu@oculus.com>
2019-01-26 16:57:28 -0800

    scene boundaries

---
+++ Assets/Scripts/SceneBoundary.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneBoundary : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Healthable healthable = col.gameObject.GetComponent<Healthable>();
+        if (healthable)
+        {
+            healthable.TakeDamage(9999, gameObject);
+        }
+        else if (col.gameObject.GetComponent<Bullet>())
+        {
+            Destroy(col.gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Healthable healthable = col.gameObject.GetComponent<Healthable>();
+        if (healthable)
+        {
+            healthable.TakeDamage(9999, gameObject);
+        }
+        else if (col.gameObject.GetComponent<Bullet>())
+        {
+            Destroy(col.gameObject);
+        }
+    }
+
+    void destroyObjects()
+    {
+
+    }
+}
