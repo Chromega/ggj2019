1b24c9fc25c37f7c49580c0f24b0de585ff4b450
Adalberto 'Nick' Garza <adagar121987@gmail.com>
2019-01-27 10:36:46 -0800

    Falling platform block

---
+++ Assets/Scripts/FallingBlock.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingBlock : MonoBehaviour
+{
+    public float timeToFall = 1.0f;
+    private bool fallActivated = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fallActivated)
+        {
+            timeToFall -= Time.deltaTime;
+            if (timeToFall <= 0)
+            {
+                this.gameObject.AddComponent<Rigidbody2D>();
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        PlayerController playerController = col.gameObject.GetComponent<PlayerController>();
+        if (playerController)
+        {
+            fallActivated = true;
+        }
+    }
+}
