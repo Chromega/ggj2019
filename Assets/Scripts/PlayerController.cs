81f53372fbebbf18ba0b4ab54e5e909342cddb12
Amber Feng <amber.feng@gmail.com>
2019-02-17 15:29:22 -0800

    Fix the post jump jig

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -52,11 +52,6 @@ public class PlayerController : Movable
     protected override float getHorizontalDirection()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
-        if (Mathf.Abs(velocity.y) > Mathf.Epsilon) // if in the middle of a jump
-        {
-            horizontal = Input.GetAxis("Horizontal");
-        }
-
 
         //lame jam code
         if (currentSustainedCast && currentSustainedCast is Shield)
